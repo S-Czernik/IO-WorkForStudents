91c2b5902016c8233fc1d992da2786c4e0e7dc76
Jakub <adamkiewiczjakub@gmail.com>
2023-12-12 13:22:05 +0100

    Zmiana tła strony startowej, zmiana strony głównej

--- IO-WorkForStudents-project/src/main/java/model/Model.java
+++ IO-WorkForStudents-project/src/main/java/model/Model.java
@@ -30,7 +30,7 @@ public class Model {
 
     public void connect() {
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ioio?useSSL=false", "root", "1234");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ioio?useSSL=false", "root", "root");
             statement = connection.createStatement();
         } catch (SQLException e) {
             System.out.println(e);
