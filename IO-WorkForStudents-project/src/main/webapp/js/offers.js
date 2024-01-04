5b7875137203f9d3111d2924bbf428cfa066d9ea
Your Name <you@example.com>
2024-01-04 21:15:13 +0100

    main page mostly sidebar things

--- IO-WorkForStudents-project/src/main/webapp/js/offers.js
+++ IO-WorkForStudents-project/src/main/webapp/js/offers.js
@@ -1,5 +1,4 @@
 window.onload = startPage(0);
-
 function startPage(pageNumber) {
     var xhttp = new XMLHttpRequest();
 
@@ -43,16 +42,64 @@ function displayOffers(offers) {
         var offer = offers[i];
 
         var offerDiv = document.createElement("div");
-        offerDiv.className = "offer";
+        offerDiv.className = "container";
         
         var titleElement = document.createElement("h2");
         titleElement.innerText = offer.title;
+        titleElement.className = "offerTitle";
 
         var contentElement = document.createElement("p");
         contentElement.innerText = offer.content;
+        contentElement.className = "offerContent";
 
+        var showMoreElement = document.createElement("button");
+        showMoreElement.innerText = "show more";
+       showMoreElement.value = "show more";
+        showMoreElement.className = "showMore";
+      //  var showMoreElement = document.createElement("button");
+        
+        
+        
         offerDiv.appendChild(titleElement);
         offerDiv.appendChild(contentElement);
+        offerDiv.appendChild(showMoreElement);
+
+        containersContainer.appendChild(offerDiv);
+    }
+}
+
+function displayOffersMainPage(offers) {
+    var containersContainer = document.getElementById("containersContainerID");
+
+    containersContainer.innerHTML = "";
+
+    for (var i = 0; i < offers.length; i++) {
+        var offer = offers[i];
+
+        var offerDiv = document.createElement("div");
+        offerDiv.className = "container";
+        
+        var titleElement = document.createElement("h2");
+        titleElement.innerText = offer.title;
+        titleElement.className = "offerTitle";
+
+        var contentElement = document.createElement("p");
+        contentElement.innerText = offer.content;
+        contentElement.className = "offerContent";
+        
+        var percentageElement = document.createElement("p");
+        percentageElement.innerText = offer.percentage;
+        percentageElement.className = "offerPercentage";
+        
+        var showMoreElement = document.createElement("button");
+        showMoreElement.innerText = "show more";
+        showMoreElement.value = "show more";
+        showMoreElement.className = "showMore";
+
+        offerDiv.appendChild(titleElement);
+        offerDiv.appendChild(percentageElement);
+        offerDiv.appendChild(contentElement);
+        offerDiv.appendChild(showMoreElement);
 
         containersContainer.appendChild(offerDiv);
     }
